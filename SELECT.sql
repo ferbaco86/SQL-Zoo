4ca0767ef41ddf386944f5716036e42237adc21f
Fernando Bahamondes <ferbaco@gmail.com>
2020-05-15 18:22:27 -0400

    Add SELECT Basics solutions

---
+++ SELECT.sql
@@ -0,0 +1,5 @@
+SELECT population FROM world WHERE name = 'Germany';
+
+SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000;
